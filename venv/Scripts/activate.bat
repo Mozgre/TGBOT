d34060be76fb602af098ce3cd7eea25cc38a57bb
Bronnice <bronnice.kurama@gmail.com>
2023-12-11 11:36:13 +0300

    init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DE2022\Desktop\qwe (1)\22-1П11\venv
+set VIRTUAL_ENV=C:\Users\DE2022\Desktop\qwe (1)\TGBOT\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
